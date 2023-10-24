95c1d4f1e13f1b925ad6ea3dc2b0ca59e0090764
pk381 <kumarprabhat0023@gmail.com>
2023-10-24 18:19:36 +0530

    update

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('prashant', 'root', 'Prabhat123@', {
     dialect: 'mysql',
-    host: '193.203.160.12'
+    host: '193.203.160.12:8443'
 });
 
 
